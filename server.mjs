86eca408cbb2c75bb1e11ba6043d39b263487e9d
misamisamisa319-web <misamisamisa319@gmail.com>
2026-01-05 17:48:07 +0900

    サーバー側で名前重複防止 + 最新チャット画面丸ごと更新

---
+++ server.mjs
@@ -0,0 +1,50 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+let users = [];
+const punishItems = ["腕立て10回", "スクワット20回", "一発ギャグ", "変顔10秒", "歌う"];
+
+io.on("connection", socket => {
+  console.log("接続:", socket.id);
+
+  socket.on("join", ({ name }) => {
+    socket.username = name;
+    users.push({ id: socket.id, name });
+    io.emit("userList", users);
+    io.emit("system", `${name} が入室しました`);
+  });
+
+  socket.on("message", data => {
+  console.log("受信:", data);
+
+  const text = data.text ?? data.message ?? data;
+
+  if (text === "罰ゲーム") {
+    const p = punishItems[Math.floor(Math.random() * punishItems.length)];
+    io.emit("system", `罰ゲーム: ${p}`);
+  } else {
+    io.emit("message", { name: socket.username, text });
+  }
+});
+
+  socket.on("leave", () => {
+    users = users.filter(u => u.id !== socket.id);
+    io.emit("userList", users);
+    if (socket.username) io.emit("system", `${socket.username} が退出しました`);
+  });
+
+  socket.on("disconnect", () => {
+    users = users.filter(u => u.id !== socket.id);
+    io.emit("userList", users);
+    if (socket.username) io.emit("system", `${socket.username} が切断されました`);
+  });
+});
+
+server.listen(3000, () => console.log("Server running on 3000"));
